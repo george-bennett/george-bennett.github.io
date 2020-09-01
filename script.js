76c500c4d2e0ca0fb0bca3b83fd96c3b71f28a30
George Bennett <george-bennett1@outlook.com>
2020-09-01 16:55:04 +0100

    New Version (Placeholder)
    Updated to use Bootstrap 5 Alpha, empty style.css and script.js for future implementation

---
+++ script.js
@@ -0,0 +1,7 @@
+// This will contain JavaScript Stuff for Website / Bootstrap 5
+
+// This is not a function
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
