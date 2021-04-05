8505e7c51206c118f87a4b359d708a156c17baef
george-bennett <george-bennett1@outlook.com>
2021-04-05 20:02:32 +0100

    new_update

--- script.js
+++ script.js
@@ -5,3 +5,8 @@
 onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.</button>
 
+const NavBarToggler = document.getElementById('toggleNav');
+
+NavBarToggler.classList.toggle('.nav-active');
+
+
