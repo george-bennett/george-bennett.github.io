0b6a75e040b8b307894debd7a9bbd260654a6eee
george-bennett <george-bennett1@outlook.com>
2021-04-06 13:09:41 +0100

    Updated Navbar and JS

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // This will contain JavaScript Stuff for Website / Bootstrap 5
 
 // This is not a function
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+const getAboutSection = document.getElementById('about');
+const getWorkSection = document.getElementById('work');
 
-const NavBarToggler = document.getElementById('toggleNav');
+const navBarAbout = document.getElementById('aboutNav');
+const navBarWork = document.getElementById('workNav');
 
-NavBarToggler.classList.toggle('.nav-active');
+function showWork() {
+    if (getWorkSection.style.display === "none") {
+        getWorkSection.style.display = "block";
+        navBarWork.className += " active"
+        navBarAbout.className = "nav-link"
 
+        getAboutSection.style.display = "none";
+    }
+}
 
+function showAbout() {
+    if (getAboutSection.style.display === "none") {
+        getAboutSection.style.display = "block";
+        navBarAbout.className += " active"
+        navBarWork.className = "nav-link"
+        getWorkSection.style.display = "none";
+    }
+}
