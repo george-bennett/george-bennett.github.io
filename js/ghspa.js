1d456d2cbee61cb6de30741eda1dfbf1045b919d
george-bennett <george-bennett1@outlook.com>
2021-04-25 12:32:36 +0100

    tweak

--- js/ghspa.js
+++ js/ghspa.js
@@ -25,11 +25,19 @@
     /* redirect all 404 trafic to index.html */
     function redirect() {
         l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
+            (l.pathname ? + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
+            (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
+            (l.hash))
+    }
+    /*
+    //Original
+    function redirect() {
+        l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
             (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
             (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
             (l.hash))
     }
-
+    */
     /* resolve 404 redirects into internal routes */
     function resolve() {
         if (l.search) {
