10c6eb896f65f11c46d60add1a53b7ddfd33d57a
george-bennett <george-bennett1@outlook.com>
2024-09-30 22:55:19 +0100

    Another modal margin change - bottom margin only

--- assets/js/main.js
+++ assets/js/main.js
@@ -388,4 +388,23 @@ closeBtn.onclick = function() {
         }
     }
 
+//Light Mode - Dark Mode
+function toggleTheme() {
+    const body = document.body;
+    const button = document.getElementById("theme-toggle");
 
+    if (body.classList.contains("light-mode")) {
+        body.classList.remove("light-mode");
+        body.classList.add("dark-mode");
+        button.textContent = "Switch to Light Mode";
+    } else {
+        body.classList.remove("dark-mode");
+        body.classList.add("light-mode");
+        button.textContent = "Switch to Dark Mode";
+    }
+}
+
+// Set initial theme
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.body.classList.add('light-mode');
+});
